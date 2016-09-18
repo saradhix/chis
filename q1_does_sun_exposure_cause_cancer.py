cce8dd783a36a9b0daa750799f45d3a8aa985576
Saradhi <saradhix@yahoo.com>
2016-09-18 00:46:05 -0700

    Baseline for task1

--- q1_does_sun_exposure_cause_cancer.py
+++ q1_does_sun_exposure_cause_cancer.py
@@ -3,8 +3,6 @@ import re
 import utils
 import sys
 
-import mylib
-
 def main():
 
   filename = 'does_sun_exposure_cause_skin_cancer_final.txt'
@@ -13,12 +11,23 @@ def main():
   num_samples = 0
   num_correct = 0
   num_supp_correct = 0
+  supp_dict={}
+  opp_dict = {}
+  neut_dict = {}
 
   for line in fd:
     (document, relevance, support) = line.split('\t')
     relevance = relevance.strip()
     support = support.strip()
     document = preprocess_doc(document)
+    if support=="support":
+      for word in document.split(' '):
+        supp_dict[word]=supp_dict.get(word,0)+1
+
+    if support=="oppose":
+      for word in document.split(' '):
+        opp_dict[word]=opp_dict.get(word,0)+1
+      
     (pred_rel, pred_supp) = classify_doc(document)
     if pred_rel == relevance:
       num_correct = num_correct + 1
@@ -31,26 +40,33 @@ def main():
   supp_accuracy = num_supp_correct / float(num_samples)
   print accuracy, supp_accuracy
 
+  #print supp_dict
+  #print opp_dict
+  #print sorted(supp_dict.items(), key=lambda x:x[1], reverse=True)
+  #print sorted(opp_dict.items(), key=lambda x:x[1], reverse=True)
 def get_support(document, relevance):
   support_words=['because', 'therefore', 'after',
-          'for', 'since', 'when', 'assuming',
+          'since', 'when', 'assuming',
           'so', 'accordingly', 'thus', 'hence',
-          'then', 'consequently','inverse']
-  oppose_words=['however', 'but', 'though',
+          'then', 'consequently', 'increase', 'intense','cause','evidence',
+'increased','harmful','develops','exposed','exposure','causes','associated','more common',
+'postulated']
+  oppose_words=['however', 'though',
           'except', 'not', 'never', 'no',
           'whereas', 'nonetheless', 'yet',
-          'despite']
+          'despite','inverse','less risk','lower risk']
 
-  keywords=['risk']
   if relevance == "irrelevant":
     return "neutral"
 
   for word in support_words:
     if word in document:
+      print word ,"present->Support"
       return "support"
 
   for word in oppose_words:
     if word in document:
+      print word ,"present->Oppose"
       return "oppose"
 
   return "neutral"
@@ -85,7 +101,7 @@ def classify_doc(document):
     for syn in synonyms[word]:
       if syn in document:
           sim = sim + 1
-          print syn
+          #print syn
           break
     if sim >=3:
       pred_rel = "relevant"
