dd9a4297d58b7086512e1fc5173701d54e6622d3
Saradhi <saradhix@yahoo.com>
2016-09-23 13:56:26 -0700

    Modified q5 using neural network

--- q5_women_should_take_hrt_post_menopause.py
+++ q5_women_should_take_hrt_post_menopause.py
@@ -2,76 +2,161 @@ import os
 import re
 import utils
 import sys
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+import numpy
+import csv
+# fix random seed for reproducibility
+seed = 0 
+numpy.random.seed(seed)
+support_words=['did not find any increase',
+        'no increase','good choice', 'less the increase', 'no evidence',
+        'flawed', 'counteract','benefits','not increase','alleviate','standard',
+        'good choice',  'neutral','ward off','slash','prevented','manage',
+        'protective','reduc','decrease','wards off','most useful','lower',
+        'most effective','effective agent','fewer cold','fighting off',
+        'take plenty',' mg','strengthens','lessens','dry up','fighting',
+        'still high','liveli','preventive', 'strengthen','improve',
+        'cut their risk','stop cancer','half','may be effective','outweigh']
+oppose_words=['risk','small','disease','alarming','deadly','not be used',
+        'stopping','inadvisable', 'cause pain','bothersome','nonhormonal',
+        'side effects','less likely', 'not help','no longer','more difficult',
+        'modest increase','cancer','collapse','more health risk','ineffective',
+        'not prevent','not beneficial','adverse','appears','not be a good choice']
+reverse_words = ['unlikely','aren','none' ]
 
+syn1 = ['this','hrt','hormone','progestin','estrogen','dose','menopaus',
+        'cancer',' it ', 'antiox','supplement','consumed','mg ', 'reduction','myth']
+syn2 = ['this', 'menopause','common cold', 'flu', 'illness','sniffles','immun',
+        'sick','throat','nose','effect', 'viral','nausea','diabetes','evidence',
+        'benefit' ]
+features = support_words + oppose_words + reverse_words + syn1 + syn2 
 def main():
 
-  filename = 'women_should_Take_HRT_Post_Menopause_final.txt'
+  train_filename = 'women_should_Take_HRT_Post_Menopause_final.txt'
+  test_filename = 'women_should_Take_HRT_Post_Menopause_test.txt'
+  results_filename = 'results/women_should_Take_HRT_Post_Menopause_result.csv'
 
-  fd = open(filename,'r')
+  fd = open(train_filename,'r')
   num_samples = 0
   num_correct = 0
   num_supp_correct = 0
-  supp_dict={}
-  opp_dict = {}
-  neut_dict = {}
+
+  X=[]
+  y1=[]
+  y2=[]
 
   for line in fd:
     (document, relevance, support) = line.split('\t')
     relevance = relevance.strip()
     support = support.strip()
     document = preprocess_doc(document)
-    (pred_rel, pred_supp) = classify_doc(document)
-    if pred_rel == relevance:
-      num_correct = num_correct + 1
-    if pred_supp == support:
-      num_supp_correct = num_supp_correct + 1
-    num_samples = num_samples + 1
-    print  num_samples, document, "PR=",pred_rel, "AR=", relevance, "PS=", pred_supp, "AS=", support
-    print "-"*30
-  accuracy = num_correct / float(num_samples)
-  supp_accuracy = num_supp_correct / float(num_samples)
-  print accuracy, supp_accuracy
 
-def get_support(document, relevance):
-  support_words=['did not find any increase','no increase','good choice', 'less the increase', 'no evidence', 'flawed', 'counteract','benefits','not increase','alleviate','standard','good choice',  'neutral','ward off','slash','prevented','manage','protective','reduc','decrease','wards off','most useful','lower','most effective','effective agent','fewer cold','fighting off','take plenty',' mg','strengthens','lessens','dry up','fighting','still high','liveli','preventive', 'strengthen','improve','cut their risk','stop cancer','half','may be effective','outweigh']
-  
-  oppose_words=['risk','small','disease','alarming','deadly','not be used', 'stopping','inadvisable', 'cause pain','bothersome','nonhormonal','side effects','less likely', 'not help','no longer','more difficult','modest increase','cancer','collapse','more health risk','ineffective', 'not prevent','not beneficial','adverse','appears','not be a good choice']
-  x=['no difference', 'wont help', 'no evidence','but not','did not','not show', 'doesnt','isnt','does not', 'little','not the best','not help','will not','disagree','unsupported','or not','not worth', 'not necessary','nor is','he same','but only for','has not been','inconclusive','nausea','diarrhea','dangerous','is not an','debunk','similar symptoms','do not recommend','do not prove','not effective','didnt help','alas','didnt','possibly','disappointing','unjustified']
+    current_x = make_x(document)
 
-  reverse_words = ['unlikely','aren','none' ]
-  reverse = False
-  support = 0
-  oppose = 0
-  if relevance == "irrelevant":
+    print current_x
+    if relevance == "relevant":
+      current_y1=1
+    else:
+      current_y1=0
+    if support == "oppose":
+      current_y2 = [1,0,0];
+    if support == "neutral":
+      current_y2 = [0,1,0];
+    if support == "support":
+      current_y2 = [0,0,1]
+
+    X.append(current_x)
+    y1.append(current_y1)
+    y2.append(current_y2)
+  fd.close()
+  X = numpy.array(X)
+  y1 = numpy.array(y1)
+  y2 = numpy.array(y2)
+  print X.shape
+  print y1.shape
+  print y2.shape
+
+  print "Number of features=",len(features)
+  model = Sequential()
+  model.add(Dense(120, input_dim=len(features), init='uniform', activation='relu'))
+  model.add(Dense(8, init='uniform', activation='relu'))
+  model.add(Dense(1, init='uniform', activation='sigmoid'))
+  # Compile model
+  model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+  # Fit the model
+  model.fit(X, y1, nb_epoch=150, batch_size=10)
+  # evaluate the model
+  scores = model.evaluate(X, y1)
+  print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+  print "Running predictions for support"
+
+  model2 = Sequential()
+  model2.add(Dense(150, input_dim=len(features), init='uniform'))
+  model2.add(Activation('tanh'))
+  model2.add(Dropout(0.5))
+  model2.add(Dense(150, init='uniform'))
+  model2.add(Activation('tanh'))
+  model2.add(Dropout(0.5))
+  model2.add(Dense(3, init='uniform'))
+  model2.add(Activation('softmax'))
+
+  #sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+  #model2.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+  model2.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+
+  model2.fit(X, y2, nb_epoch=150, batch_size=10)
+  score = model2.evaluate(X, y2, batch_size=10)
+  print("%s: %.2f%%" % (model2.metrics_names[1], score[1]*100))
+
+  #Do predictions on test
+  X=[]
+  y1=[]
+  y2=[]
+  documents=[]
+  fd = open(test_filename,'r')
+  for line in fd:
+    document = line.strip()
+    documents.append(document)
+    document = preprocess_doc(document)
+    current_x = make_x(document)
+    X.append(current_x)
+
+  relevances = model.predict(X)
+  supports = model2.predict(X)
+
+  relevances = [int(round(x)) for x in relevances]
+  print relevances
+  new_supports = [0 for i in supports]
+  for (i,support) in enumerate(supports):
+    new_supports[i] = vec_to_class(support)
+  print new_supports
+
+  fd = open(results_filename,'w')
+  datawriter = csv.writer(fd)
+  for (i,document) in enumerate(documents):
+    datawriter.writerow([document, label_name_y1(relevances[i]), label_name_y2(new_supports[i])])
+
+
+def vec_to_class(support):
+  support = support.tolist()
+  return support.index(max(support))
+
+def label_name_y1(label):
+  if label==0:
+    return "irrelevant"
+  else:
+    return "relevant"
+
+def label_name_y2(label):
+  if label==0:
+    return "oppose"
+  elif label==1:
     return "neutral"
-
-  for word in support_words:
-    if word in document:
-      print word ,"present->Support"
-      support = support + 1 
-
-  for word in oppose_words:
-    if word in document:
-      print word ,"present->Oppose"
-      oppose = oppose + 1
-  
-  for word in reverse_words:
-#    print "Checking for word", word
-    if word in document:
-      print word,"Reversed meaning", "s=",support,"o=",oppose
-      reverse=True
-  if reverse and oppose:
+  else:
     return "support"
-  if reverse and support:
-    return "oppose"
-
-  if support:
-    return "support"
-  if oppose:
-    return "oppose"
-
-  return "neutral"
-
 
 
 def preprocess_doc(doc):
@@ -82,33 +167,13 @@ def preprocess_doc(doc):
 
   #Special character removal
   doc = re.sub('\W+', ' ', doc)
-  doc = doc.replace('won t', 'wont')
-  doc = doc.replace('doesn t', 'doesnt')
-  doc = doc.replace('isn t', 'isnt')
-  doc = doc.replace('didn t', 'didnt')
   return doc
 
-def classify_doc(document):
-  groups = ['hrt']
-
-  synonyms={}
-
-  synonyms['hrt']=['this','hrt','hormone','progestin','estrogen','dose','menopaus','cancer',' it ', 'antiox','supplement','consumed','mg ', 'reduction','myth']
-  synonyms['menopause']=['this', 'menopause','common cold', 'flu', 'illness','sniffles','immun','sick','throat','nose','effect', 'viral','nausea','diabetes','evidence', 'benefit' ]
-  q = 'does vitamin C prevent common cold'
-  sim=0
-  for word in groups:
-    for syn in synonyms[word]:
-      if syn in document:
-          sim = sim + 1
-          print "Synonym=",syn
-          break
-    if sim >=1:
-      pred_rel = "relevant"
-    else:
-      pred_rel = "irrelevant"
-    pred_supp = get_support(document, pred_rel)
-  return (pred_rel, pred_supp)
-
+def make_x(document):
+  current_x = [0 for f in features]
+  for (i, feature) in enumerate(features):
+    if feature in document:
+      current_x[i]=1
+  return current_x
 if __name__ == "__main__":
   main() 
